56a0bb38fff2703d7794919507399fcf5f44d70d
Allan Chain <allanchain@pku.edu.cn>
2020-01-07 19:58:58 +0800

    fix hash bug

--- assets/js/hash.js
+++ assets/js/hash.js
@@ -1,15 +1,23 @@
-function hashChange(event) {
+function hashChange(event, inited) {
   let hash = decodeURI(location.hash);
   // Do nothing if is the symbol of done
-  if (hash == "#") return;
+  // unless is the first call;
+  if (hash.charAt(hash.length-1) == "~"){
+    if (inited) return;
+    else hash = hash.slice(0, -1);
+  }
   let header = $(hash + "-nofocus");
   if (header.length) {
     $('html, body').animate({
       scrollTop: header.offset().top - 70
     }, "slow");
+    mgr.mobileHideMenu();
     // Setting hash to `#` will automatically collapse menu?
-    // Interesting...
-    location.hash = "#";
+    // No, the browser will first jump to top,
+    // but the js animation will drag it down again.
+    // But this will introduce some flashing bug.
+    // So we will try adding a `~` suffix
+    location.hash = hash + "~";
   }
 }
 $(function () {
@@ -17,5 +25,5 @@ $(function () {
   // hacking shoud be taken over by js
   $("main :header").each((index, header) => header.id += "-nofocus");
   // do scrolling if is linked by hash
-  hashChange();
+  hashChange(true);
 });
